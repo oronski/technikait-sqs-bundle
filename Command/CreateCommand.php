2e0a09e95f7548d3b34aa8b068fbda1a109d1c7a
Wojtek Oroński <w.oronski@acrux.net.pl>
2017-09-15 09:27:33 +0100

    Create commad

---
+++ Command/CreateCommand.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace TechnikaIt\SqsBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TechnikaIt\SqsBundle\Service\QueueManager;
+
+/**
+ * Class QueueCreateCommand
+ * @package TechnikaIt\SqsBundle\Command
+ */
+class QueueCreateCommand extends ContainerAwareCommand
+{
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('TechnikaIt:sqs:create')
+            ->addArgument(
+                'name',
+                InputArgument::REQUIRED,
+                'Queue ID which you want to send message'
+            )
+            ->addOption(
+                'delay_seconds',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'DelaySeconds',
+                0
+            )
+            ->addOption(
+                'maximum_message_size',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'MaximumMessageSize',
+                262144
+            )
+            ->addOption(
+                'message_retention_period',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'MessageRetentionPeriod',
+                345600
+            )
+            ->addOption(
+                'receive_message_wait_time_seconds',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'ReceiveMessageWaitTimeSeconds',
+                0
+            )
+            ->addOption(
+                'visibility_timeout',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'VisibilityTimeout',
+                30
+            )
+            ->setDescription('Create a queue by name and basic attributions');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $queueName = $input->getArgument('name');
+        if ($this->getContainer()->has(sprintf('TechnikaIt.sqs.%s', $queueName))) {
+            throw new \InvalidArgumentException(sprintf('Queue [%s] exists. Please use another name.', $queueName));
+        }
+
+        $io = new SymfonyStyle($input, $output);
+        $io->title(sprintf('Start creating a new queue which name is <comment>%s</comment>', $queueName));
+
+        /** @var QueueManager $queueManager */
+        $queueManager = $this->getContainer()->get('TechnikaIt.sqs.queue_manager');
+        $queueUrl = $queueManager->createQueue($queueName, [
+            'DelaySeconds' => $input->getOption('delay_seconds'),
+            'MaximumMessageSize' => $input->getOption('maximum_message_size'),
+            'MessageRetentionPeriod' => $input->getOption('message_retention_period'),
+            'ReceiveMessageWaitTimeSeconds' => $input->getOption('receive_message_wait_time_seconds'),
+            'VisibilityTimeout' => $input->getOption('visibility_timeout')
+        ]);
+
+        $io->text(sprintf('Success. New Queue URL: <comment>%s</comment>', $queueUrl));
+        $io->success('Done');
+    }
+}
